8b2f43d23f61cd3147cc9a1bba87322f73fb7259
lhf-nife <lhf6157@163.com>
2017-08-14 11:19:15 +0800

    no message

--- src/components/checkbox/CheckboxGroup.js
+++ src/components/checkbox/CheckboxGroup.js
@@ -81,7 +81,6 @@ class CheckboxGroup extends PureComponent {
   toggleOption=(option) => {
     const optionIndex = this.state.value.indexOf(option.value);s
     const value = [...this.state.value];
-console.log(option,222,optionIndex);
     if (optionIndex === -1) {
       value.push(option.value);
     } else {
