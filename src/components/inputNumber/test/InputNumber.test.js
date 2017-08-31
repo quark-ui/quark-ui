01b6122778c98be0fdd314971e361d67f970671f
yanwei <1061691060@qq.com>
2017-08-31 17:05:51 +0800

    add test

---
+++ src/components/inputNumber/test/InputNumber.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { shallow, mount, render } from 'enzyme';
+import { expect, should } from 'chai';
+import RcInputNumber from 'rc-input-number';
+import InputNumber from '../InputNumber';
+import styles from '../InputNumber.css';
+
+describe('inputNumber-test-describe----------', () => {
+  it('inputNumber render',()=>{
+    const app = mount(<InputNumber style={{ width: 200 }} min={1} max={10} defaultValue={3} disabled />);
+    console.log(app.debug());
+    expect(app.hasClass(styles['input-number-normal'])).to.equal(false);
+  });
+});
+
