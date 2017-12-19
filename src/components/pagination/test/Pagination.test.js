ef29f41cb8be69065689554d7e4f5083ce642ae9
october <1061691060@qq.com>
2017-12-19 09:51:18 +0800

    change input test && change pagination test (#57)
    * change input demo type
    
    * set select defaultValue
    
    * merge
    
    * merge
    
    * set select defaultValue
    
    * change inputNumber & tabs
    
    * change select
    
    * change select hover css
    
    * change select
    
    * change select
    
    * change progress
    
    * change inputNumber demo css
    
    * add notification
    
    * change notice
    
    * change notification
    
    * change notification
    
    * change notice
    
    * change modal test
    
    * add test
    
    * add test
    
    * add test
    
    * modal
    
    * change package
    
    * change input test
    
    * change test
    
    * add test
    
    * add test
    
    * add tes
    
    * add test
    
    * change list
    
    * change test
    
    * change trigger
    
    * change test
    
    * change test
    
    * change test
    
    * change notification && test
    
    * change radio test
    
    * change olivianate to october-yan
    
    * change tab
    
    * add notification test
    
    * change notification test
    
    * change pagination test && change notification test
    
    * change pagination test && change notification test
    
    * change pagination test
    
    * merge notification test
    
    * change input test && change pagination test
    
    * change input test
    
    * change input test && change steps test

--- src/components/pagination/test/Pagination.test.js
+++ src/components/pagination/test/Pagination.test.js
@@ -12,12 +12,12 @@ describe('pagination-test-describe----------', () => {
     expect(app.find('li').length).to.equal(0);
     const app2 = shallow(<Pagination current={3} total={1} />);
     let item = app2.find('li').first().children();
-    expect(item.hasClass(styles['pagination__item--active'])).to.equal(true);//第一个选中
+    expect(item.hasClass(styles['pagination__item--active'])).to.equal(true); // 第一个选中
     expect(app2.find('li').length).to.equal(1);
     const app3 = shallow(<Pagination current={5} total={50} />);
     expect(app3.find('li').length).to.equal(5);
     item = app3.find('li').last().children();
-    expect(item.hasClass(styles['pagination__item--active'])).to.equal(true);//最后一个选中
+    expect(item.hasClass(styles['pagination__item--active'])).to.equal(true);// 最后一个选中
   });
 
 
@@ -27,15 +27,15 @@ describe('pagination-test-describe----------', () => {
       total={50}
     />);
     expect(app.find(`.${styles['pagination--normal']}`).length).to.equal(1);
-    expect(app.find('li').length).to.equal(5);//总共5页
+    expect(app.find('li').length).to.equal(5);// 总共5页
     let item = app.find('li').first().children();
-    expect(item.hasClass(styles['pagination__item--active'])).to.equal(false);//第一个未选中
+    expect(item.hasClass(styles['pagination__item--active'])).to.equal(false);// 第一个未选中
     item.simulate('click');
-    expect(item.hasClass(styles['pagination__item--active'])).to.equal(true);//第一个选中
+    expect(item.hasClass(styles['pagination__item--active'])).to.equal(true);// 第一个选中
     item = app.find('li').last().children();
-    expect(item.hasClass(styles['pagination__item--active'])).to.equal(false);//最后一个未选中
+    expect(item.hasClass(styles['pagination__item--active'])).to.equal(false);// 最后一个未选中
     item.simulate('click');
-    expect(item.hasClass(styles['pagination__item--active'])).to.equal(true);//最后一个选中
+    expect(item.hasClass(styles['pagination__item--active'])).to.equal(true);// 最后一个选中
   });
 
   it('pagination---------------first button click', () => {
@@ -47,7 +47,7 @@ describe('pagination-test-describe----------', () => {
     const button = app.find('button').first().children();
     button.simulate('click');
     const item = app.find('li').first().children();
-    expect(item.hasClass(styles['pagination__item--active'])).to.equal(true);//点击往前一页，第一个选中
+    expect(item.hasClass(styles['pagination__item--active'])).to.equal(true);// 点击往前一页，第一个选中
   });
 
   it('pagination---------------last button click', () => {
@@ -60,7 +60,7 @@ describe('pagination-test-describe----------', () => {
     const button = app.find('button').last().children();
     button.simulate('click');
     const item = app.find('li').last().children();
-    expect(item.hasClass(styles['pagination__item--active'])).to.equal(true);//点击往后一页，最后一个选中
+    expect(item.hasClass(styles['pagination__item--active'])).to.equal(true);// 点击往后一页，最后一个选中
   });
 
   it('pagination set  size small', () => {
@@ -81,20 +81,20 @@ describe('pagination-test-describe----------', () => {
     expect(spy.calledOnce).to.equal(false);
     app.setProps({ current : 4 });
     expect(spy.calledOnce).to.equal(true);
-    expect(app.find(`.${styles['pagination--small']}`).length).to.equal(1);//迷你
-    expect(app.find('span').length).to.equal(2);//共计多少页和翻页
+    expect(app.find(`.${styles['pagination--small']}`).length).to.equal(1);// 迷你
+    expect(app.find('span').length).to.equal(2);// 共计多少页和翻页
     app.find('input').value = 1;
     let input = app.find('input').get(0);
     input.value = '1';
-    app.find('input').first().simulate('keyPress', { key: 'Enter' });//输入页码1回车
+    app.find('input').first().simulate('keyPress', { key: 'Enter' });// 输入页码1回车
     const item = app.find('li').first().children();
-    expect(item.hasClass(styles['pagination__item--active'])).to.equal(true); //第一个选中
+    expect(item.hasClass(styles['pagination__item--active'])).to.equal(true); // 第一个选中
     input = app.find('input').get(0);
     input.value = 'a';
     app.find('input').first().simulate('keyPress', { key: 'Enter' });
-    expect(item.hasClass(styles['pagination__item--active'])).to.equal(true); //第一个选中
+    expect(item.hasClass(styles['pagination__item--active'])).to.equal(true); // 第一个选中
 
-    app.find('select').simulate('change');//选择每页的数量
+    app.find('select').simulate('change');// 选择每页的数量
   });
 });
 
